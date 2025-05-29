967981368bc244eb49e25a3638f971305132ffa7
Ingrid Abraham <git@ingrids.email>
2025-05-29 15:54:02 +0200

    add code of conduct to signal page

--- src/web.rs
+++ src/web.rs
@@ -22,7 +22,7 @@ use serde::Deserialize;
 use std::str::FromStr;
 
 const STYLESHEET_HOME: &str = "home.css?v=1.2";
-const STYLESHEET_SIGNAL: &str = "signal.css?v=1.2";
+const STYLESHEET_SIGNAL: &str = "signal.css?v=1.3";
 const STYLESHEET_ADMIN: &str = "admin.css?v=1.2";
 const STYLESHEET_FORM: &str = "form.css?v=1.2";
 
